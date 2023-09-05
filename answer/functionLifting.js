be2bc1f11e0ef46bb6c35b00e0c8ea5cdbff1451
DESKTOP-GGICTGS\DeLL <pengxu9515@163.com>
2023-09-05 19:17:43 +0800

    作用域

---
+++ answer/functionLifting.js
@@ -0,0 +1,36 @@
+
+/*
+
+未使用 var const let 的函数声明 a 会被提升至块作用域顶部并赋值（即 词法环境中blockScope.a=f()）同时提升至全局顶部 默认值window.a=undefined
+代码执行 函数声明 行时 将块作用域内 blockScope.a 的值赋给全局变量window.a (window.a = blockScope.a)
+
+！不要在块内 进行函数声明 污染全局
+
+相关：作用域 scope.js
+
+*/
+{
+    console.log(a)
+    a = 1
+    function a(){ }
+    a = 2
+    function a(){ }
+    a = 3
+    console.log(a) 
+}
+console.log(a)
+
+/*
+result:
+f a(){}
+3
+2
+
+相关知识：作用域 scope.js
+
+未使用 var const let 的函数声明 a 会被提升至块作用域顶部并赋值（即 词法环境中blockScope.a=f()）同时提升至全局顶部 默认值window.a=undefined
+代码执行 函数声明 行时 将块作用域内 blockScope.a 的值赋给全局变量window.a (window.a = blockScope.a)
+
+！不要在块内 进行函数声明 污染全局
+
+*/
---
+++ answer/scope.js
@@ -0,0 +1,42 @@
+/*
+js 作用域 scope 有四种 
+
+全局作用域 global 
+var 在全局声明的变量 作用域为 global
+
+局部作用域 local 
+函数内部声明的变量 作用域为 local
+
+脚本作用域 script
+let const 在全局声明的变量 作用域为 script
+
+块作用域 block 
+let const 在{}中(不包含function(){})声明的变量 作用域为 block
+
+
+相关：
+左右查询 leftQueryRightQuery.js
+*/
+
+var a_global_var 
+let a_script_let 
+const a_script_const = 1
+
+function a_scriptScope(){
+  var a_local_var
+  let a_local_let
+  const a_local_const = 2
+  {
+    var a_local_var_2
+    let a_block_let
+    const a_block_const = 3
+  }
+  let a_scriptScope2= function(){
+    console.log(a_scriptScope2);
+    let a_local_let_2
+    const a_local_const_2 = 4
+  }
+  a_scriptScope2()
+}
+
+a_scriptScope()
