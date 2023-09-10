b5b91b208d8a7e1aa209a7b75d7359957eb47056
hao <568131946@qq.com>
2023-09-10 09:53:50 +0800

    myCall

--- myFun/myCall.js
+++ myFun/myCall.js
@@ -6,6 +6,7 @@ myApply.js
 
 
 Function.prototype.myCall = function(el,...args){
+  if(this instanceof Function.prototype.myCall) throw Error('this is not a constructor')
   if(typeof this !== 'function') throw Error(this+' is not a function')
   let obj = [null,undefined].includes(el) ? globalThis : typeof el !== 'object' ? Object(el) : el
   let temp = Symbol('temp')
@@ -19,12 +20,12 @@ Function.prototype.myCall = function(el,...args){
 }
 
 function sum(a,b){
-  console.log('this',this);
   return a + b
 }
 let obj = {
   a:1,
   b:2
 }
-console.log(obj.myCall(obj,2,3))
-console.log(obj.call(obj,2,3))
+console.log(new sum.myCall(obj,2,3))
+console.log(sum.myCall(obj,2,3))
+console.log(new sum.call(obj,2,3))
